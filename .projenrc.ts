454d0d196b558e559b1723fc47d4f4c004827794
Bryan Galvin <bcgalvin@gmail.com>
2024-10-20 15:54:08 -0700

    initial projen setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,43 @@
-import { cdk8s } from 'projen';
+import { cdk8s, javascript } from 'projen';
+
+const commonIgnore = ['.vscode/', '.idea/'];
+const devDeps = [
+  'aws-cdk',
+  'constructs',
+  'aws-cdk-lib',
+  'cdk8s-cli',
+  'fs-extra',
+  '@types/fs-extra',
+];
+const peerDeps = ['aws-cdk-lib', 'cdk8s', 'constructs'];
+
 const project = new cdk8s.ConstructLibraryCdk8s({
   author: 'Bryan Galvin',
   authorAddress: 'bcgalvin@gmail.com',
-  cdk8sVersion: '1.4.10',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.5.0',
   name: 'metaflow-blueprints',
-  projenrcTs: true,
   repositoryUrl: 'https://github.com/bcgalvin/metaflow-blueprints.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  defaultReleaseBranch: 'main',
+  cdk8sVersion: '2.69.9',
+  typescriptVersion: '~5.5.0',
+  jsiiVersion: '~5.5.0',
+  projenrcTs: true,
+  peerDeps: peerDeps,
+  devDeps: devDeps,
+  prettier: true,
+  eslint: true,
+  prettierOptions: {
+    settings: {
+      endOfLine: javascript.EndOfLine.AUTO,
+      singleQuote: true,
+      tabWidth: 2,
+      trailingComma: javascript.TrailingComma.ALL,
+    },
+  },
+  eslintOptions: {
+    dirs: ['src'],
+    ignorePatterns: ['examples/**/*', 'test/*.snapshot/**/*', '*.d.ts'],
+    devdirs: ['test'],
+  },
+  gitignore: commonIgnore,
 });
 project.synth();

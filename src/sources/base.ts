6595728fa8c513611609d0e40ff129fdf4391aa2
Bryan Galvin <bcgalvin@gmail.com>
2024-10-21 09:46:51 -0700

    refactor initial implementation of the abc for the imported EventSource resource from the argo events crd to provide an escape hatch for the underlying k8s api object and allow for adding dependencies - e.g. s3 event source (via sqs) depends on s3 bucket

--- src/sources/base.ts
+++ src/sources/base.ts
@@ -1,27 +1,69 @@
-import { ApiObject, ApiObjectMetadata, JsonPatch } from 'cdk8s';
+import { ApiObjectMetadata } from 'cdk8s';
 import { Construct } from 'constructs';
+import { SqsEventSourceSpec } from './sqs';
+import { EventSource } from '../imports/argoproj.io';
 
-export interface EventSourceProps {
+/**
+ * Union type representing all possible event source specifications.
+ * Currently only SQS but will be the union of all possible event sources.
+ */
+export type EventSourceSpec = SqsEventSourceSpec;
+
+/**
+ * Properties for creating a base event source.
+ */
+export interface BaseEventSourceProps {
+  /** Metadata for the event source. */
   readonly metadata: ApiObjectMetadata;
-  readonly spec: any;
+  /** Specification for the event source. */
+  readonly spec: EventSourceSpec;
 }
 
-export abstract class BaseEventSource extends ApiObject {
-  public static readonly GVK = {
-    apiVersion: 'argoproj.io/v1alpha1',
-    kind: 'EventSource',
-  };
-  constructor(scope: Construct, id: string, props: EventSourceProps) {
+/**
+ * Abstract base class for all event sources.
+ */
+export abstract class BaseEventSource extends EventSource {
+  protected readonly eventSource: EventSource;
+
+  /**
+   * Creates a new BaseEventSource.
+   * @param scope The scope in which to define this construct
+   * @param id The scoped construct ID
+   * @param props Configuration properties for the event source
+   */
+  constructor(scope: Construct, id: string, props: BaseEventSourceProps) {
     super(scope, id, {
-      ...BaseEventSource.GVK,
+      ...EventSource.GVK,
       ...props,
+    });
+
+    this.eventSource = new EventSource(this, 'EventSource', {
+      metadata: props.metadata,
+      spec: this.generateSpec(props.spec),
     });
   }
 
-  protected addToSpec(key: string, value: any): void {
-    const currentSpec = this.toJson().spec || {};
-    this.addJsonPatch(
-      JsonPatch.replace('/spec', { ...currentSpec, [key]: value }),
-    );
+  /**
+   * Generates the spec for the event source.
+   * @param spec The event source specification
+   * @returns The generated spec object
+   */
+  protected abstract generateSpec(spec: EventSourceSpec): {
+    [key: string]: any;
+  };
+
+  /**
+   * Adds a dependency to the event source.
+   * @param dependency The construct to depend on
+   */
+  public addDependency(dependency: Construct): void {
+    this.eventSource.addDependency(dependency);
+  }
+
+  /**
+   * Returns the underlying EventSource object for advanced customization.
+   */
+  public get apiObject(): EventSource {
+    return this.eventSource;
   }
 }
